402e4c254c221841a479d17fed8ec7dd882ad19a
Robert Rostohar <Robert.Rostohar@arm.com>
2020-05-20 14:15:45 +0200

    Add AWS MQTT Demo for STM32L562E-DK board (TrustZone disabled)
    AWS Demo for STM32L5 device for STM32L562E-DK board:
     - fully functional demo (with debug)
     - TrustZone disabled
     - adopted from aws_mqtt_demo project (non-secure side)
     - details in Abstract.txt
     - supports WiFi modules: ESP8266, WiZFi360, ISM43362
     - code organized and split into layers
     - exported .cprj files for each target (one per WiFi module)

---
+++ project/AWS_MQTT_Demo/STM32L562E-DK_NoTZ/RTE/_STM32L562-DK_WizFi360/Pre_Include_Global.h
@@ -0,0 +1,18 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'aws_mqtt_demo' 
+ * Target:  'STM32L562-DK WizFi360' 
+ */
+
+#ifndef PRE_INCLUDE_GLOBAL_H
+#define PRE_INCLUDE_GLOBAL_H
+
+/*  MDK-Packs::IoT Utility:AWS:Platform:CMSIS-RTOS2 mbedTLS:1.0.0 */
+#define IOT_SYSTEM_TYPES_FILE "iot_platform_types_cmsis.h"
+        #define MBEDTLS_CONFIG_FILE "iot_config_mbedtls.h"
+
+
+#endif /* PRE_INCLUDE_GLOBAL_H */
