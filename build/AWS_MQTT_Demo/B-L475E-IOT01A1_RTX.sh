8a67142901e86bfd26d06271cf07b9ce7cf23dba
Robert Rostohar <Robert.Rostohar@arm.com>
2020-05-21 16:39:12 +0200

    Add build scripts for AWS MQTT Demo on B-L475E-IOT01A1 board
    Build scripts for composing the complete project:
     - two scripts: board layer with RTX or FreeRTOS
     - using cbuildgen compose with required layers
     - display instructions for updating the project
       - AWS connection settings
       - WiFi connection settings
     - display instructions to build with cbuild (optional)
     - display instruction to build, run and debug with uVision

---
+++ build/AWS_MQTT_Demo/B-L475E-IOT01A1_RTX.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+BOARD=B-L475E-IOT01A1
+RTOS=RTX
+
+echo "Generate project AWS_MQTT_Demo"
+echo "  Board:  "$BOARD
+echo "  RTOS:   "$RTOS
+echo
+
+mkdir -p "${BOARD}_${RTOS}"
+pushd "${BOARD}_${RTOS}"
+cbuildgen compose aws_mqtt_demo.cprj \
+ ../../../layer/Application/AWS_MQTT_Demo/Application.clayer \
+ ../../../layer/Cloud/AWS/Cloud.clayer \
+ ../../../layer/Socket/WiFi/Socket.clayer \
+ ../../../layer/Board/${BOARD}_${RTOS}/Board.clayer
+popd
+
+echo
+echo "Manually update project"
+echo " - RTE/IoT_Client/iot_config.h"
+echo "     AWS connection settings (see Abstract.txt)"
+echo " - network.c"
+echo "     WiFi connection settings (see Abstract.txt)"
+echo
+echo "Build project (optional)"
+echo "  cbuild.sh aws_mqtt_demo.cprj"
+echo
+echo "Run project"
+echo " - open aws_mqtt_demo.cprj (uVision)"
+echo " - if already built with cbuild"
+echo "   - change folder for Objects: Objects"
+echo "   - change folder for Listings: Listings"
+echo " - if building with uVision"
+echo "   - STM32CubeMX:project.script type=Text"
+echo " - select ST-Link Debugger"
+echo "     Core Clock: 80 MHz"
+echo "     Trace Enable"
+echo " - Start Debug Session"
+echo " - Open Debug (printf) Viewer"
+echo " - Run Target"
