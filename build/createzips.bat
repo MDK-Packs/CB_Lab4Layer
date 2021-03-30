92aabf5b1c90704532a318a7fd4bd45da8ce1c6d
Christopher Seidl <KeilChris@users.noreply.github.com>
2021-03-30 14:09:41 +0200

    Added files to create current Keil Studio demo prjects

---
+++ build/createzips.bat
@@ -0,0 +1,304 @@
+@echo off
+
+REM Creating AWS Project ZIP Files
+
+pushd AWS_MQTT_Demo\LPCXpresso55S69_ESP8266_WiFi_FreeRTOS
+"C:\Program Files\7-ZIP\7z" a -tzip AWS_MQTT_Demo_LPCXpresso55S69_ESP8266_WiFi_FreeRTOS.zip *
+move AWS_MQTT_Demo_LPCXpresso55S69_ESP8266_WiFi_FreeRTOS.zip C:\Temp
+popd
+
+pushd AWS_MQTT_Demo\LPCXpresso55S69_ESP8266_WiFi_RTX
+"C:\Program Files\7-ZIP\7z" a -tzip AWS_MQTT_Demo_LPCXpresso55S69_ESP8266_WiFi_RTX.zip *
+move AWS_MQTT_Demo_LPCXpresso55S69_ESP8266_WiFi_RTX.zip C:\Temp
+popd
+
+pushd AWS_MQTT_Demo\LPCXpresso55S69_ISM43362_WiFi_FreeRTOS
+"C:\Program Files\7-ZIP\7z" a -tzip AWS_MQTT_Demo_LPCXpresso55S69_ISM43362_WiFi_FreeRTOS.zip *
+move AWS_MQTT_Demo_LPCXpresso55S69_ISM43362_WiFi_FreeRTOS.zip C:\Temp
+popd
+
+pushd AWS_MQTT_Demo\LPCXpresso55S69_ISM43362_WiFi_RTX
+"C:\Program Files\7-ZIP\7z" a -tzip AWS_MQTT_Demo_LPCXpresso55S69_ISM43362_WiFi_RTX.zip *
+move AWS_MQTT_Demo_LPCXpresso55S69_ISM43362_WiFi_RTX.zip C:\Temp
+popd
+
+pushd AWS_MQTT_Demo\LPCXpresso55S69_WizFi360_WiFi_FreeRTOS
+"C:\Program Files\7-ZIP\7z" a -tzip AWS_MQTT_Demo_LPCXpresso55S69_WizFi360_WiFi_FreeRTOS.zip *
+move AWS_MQTT_Demo_LPCXpresso55S69_WizFi360_WiFi_FreeRTOS.zip C:\Temp
+popd
+
+pushd AWS_MQTT_Demo\LPCXpresso55S69_WizFi360_WiFi_RTX
+"C:\Program Files\7-ZIP\7z" a -tzip AWS_MQTT_Demo_LPCXpresso55S69_WizFi360_WiFi_RTX.zip *
+move AWS_MQTT_Demo_LPCXpresso55S69_WizFi360_WiFi_RTX.zip C:\Temp
+popd
+
+pushd AWS_MQTT_Demo\MIMXRT1064-EVK_ESP8266_WiFi_FreeRTOS
+"C:\Program Files\7-ZIP\7z" a -tzip AWS_MQTT_Demo_MIMXRT1064-EVK_ESP8266_WiFi_FreeRTOS.zip *
+move AWS_MQTT_Demo_MIMXRT1064-EVK_ESP8266_WiFi_FreeRTOS.zip C:\Temp
+popd
+
+pushd AWS_MQTT_Demo\MIMXRT1064-EVK_ESP8266_WiFi_RTX
+"C:\Program Files\7-ZIP\7z" a -tzip AWS_MQTT_Demo_MIMXRT1064-EVK_ESP8266_WiFi_RTX.zip *
+move AWS_MQTT_Demo_MIMXRT1064-EVK_ESP8266_WiFi_RTX.zip C:\Temp
+popd
+
+pushd AWS_MQTT_Demo\MIMXRT1064-EVK_MW-Network_ETH_RTX
+"C:\Program Files\7-ZIP\7z" a -tzip AWS_MQTT_Demo_MIMXRT1064-EVK_ISM43362_MW-Network_ETH_RTX.zip *
+move AWS_MQTT_Demo_MIMXRT1064-EVK_MW-Network_ETH_RTX.zip C:\Temp
+popd
+
+pushd AWS_MQTT_Demo\MIMXRT1064-EVK_WizFi360_WiFi_FreeRTOS
+"C:\Program Files\7-ZIP\7z" a -tzip AWS_MQTT_Demo_MIMXRT1064-EVK_WizFi360_WiFi_FreeRTOS.zip *
+move AWS_MQTT_Demo_MIMXRT1064-EVK_WizFi360_WiFi_FreeRTOS.zip C:\Temp
+popd
+
+pushd AWS_MQTT_Demo\MIMXRT1064-EVK_WizFi360_WiFi_RTX
+"C:\Program Files\7-ZIP\7z" a -tzip AWS_MQTT_Demo_MIMXRT1064-EVK_WizFi360_WiFi_RTX.zip *
+move AWS_MQTT_Demo_MIMXRT1064-EVK_WizFi360_WiFi_RTX.zip C:\Temp
+popd
+
+
+
+REM Creating Azure Project ZIP Files
+
+pushd Azure_MQTT_Demo\LPCXpresso55S69_ESP8266_WiFi_FreeRTOS
+"C:\Program Files\7-ZIP\7z" a -tzip Azure_MQTT_Demo_LPCXpresso55S69_ESP8266_WiFi_FreeRTOS.zip *
+move Azure_MQTT_Demo_LPCXpresso55S69_ESP8266_WiFi_FreeRTOS.zip C:\Temp
+popd
+
+pushd Azure_MQTT_Demo\LPCXpresso55S69_ESP8266_WiFi_RTX
+"C:\Program Files\7-ZIP\7z" a -tzip Azure_MQTT_Demo_LPCXpresso55S69_ESP8266_WiFi_RTX.zip *
+move Azure_MQTT_Demo_LPCXpresso55S69_ESP8266_WiFi_RTX.zip C:\Temp
+popd
+
+pushd Azure_MQTT_Demo\LPCXpresso55S69_ISM43362_WiFi_FreeRTOS
+"C:\Program Files\7-ZIP\7z" a -tzip Azure_MQTT_Demo_LPCXpresso55S69_ISM43362_WiFi_FreeRTOS.zip *
+move Azure_MQTT_Demo_LPCXpresso55S69_ISM43362_WiFi_FreeRTOS.zip C:\Temp
+popd
+
+pushd Azure_MQTT_Demo\LPCXpresso55S69_ISM43362_WiFi_RTX
+"C:\Program Files\7-ZIP\7z" a -tzip Azure_MQTT_Demo_LPCXpresso55S69_ISM43362_WiFi_RTX.zip *
+move Azure_MQTT_Demo_LPCXpresso55S69_ISM43362_WiFi_RTX.zip C:\Temp
+popd
+
+pushd Azure_MQTT_Demo\LPCXpresso55S69_WizFi360_WiFi_FreeRTOS
+"C:\Program Files\7-ZIP\7z" a -tzip Azure_MQTT_Demo_LPCXpresso55S69_WizFi360_WiFi_FreeRTOS.zip *
+move Azure_MQTT_Demo_LPCXpresso55S69_WizFi360_WiFi_FreeRTOS.zip C:\Temp
+popd
+
+pushd Azure_MQTT_Demo\LPCXpresso55S69_WizFi360_WiFi_RTX
+"C:\Program Files\7-ZIP\7z" a -tzip Azure_MQTT_Demo_LPCXpresso55S69_WizFi360_WiFi_RTX.zip *
+move Azure_MQTT_Demo_LPCXpresso55S69_WizFi360_WiFi_RTX.zip C:\Temp
+popd
+
+pushd Azure_MQTT_Demo\MIMXRT1064-EVK_ESP8266_WiFi_FreeRTOS
+"C:\Program Files\7-ZIP\7z" a -tzip Azure_MQTT_Demo_MIMXRT1064-EVK_ESP8266_WiFi_FreeRTOS.zip *
+move Azure_MQTT_Demo_MIMXRT1064-EVK_ESP8266_WiFi_FreeRTOS.zip C:\Temp
+popd
+
+pushd Azure_MQTT_Demo\MIMXRT1064-EVK_ESP8266_WiFi_RTX
+"C:\Program Files\7-ZIP\7z" a -tzip Azure_MQTT_Demo_MIMXRT1064-EVK_ESP8266_WiFi_RTX.zip *
+move Azure_MQTT_Demo_MIMXRT1064-EVK_ESP8266_WiFi_RTX.zip C:\Temp
+popd
+
+pushd Azure_MQTT_Demo\MIMXRT1064-EVK_MW-Network_ETH_FreeRTOS
+"C:\Program Files\7-ZIP\7z" a -tzip Azure_MQTT_Demo_MIMXRT1064-EVK_ISM43362_MW-Network_ETH_FreeRTOS.zip *
+move Azure_MQTT_Demo_MIMXRT1064-EVK_MW-Network_ETH_FreeRTOS.zip C:\Temp
+popd
+
+pushd Azure_MQTT_Demo\MIMXRT1064-EVK_MW-Network_ETH_RTX
+"C:\Program Files\7-ZIP\7z" a -tzip Azure_MQTT_Demo_MIMXRT1064-EVK_ISM43362_MW-Network_ETH_RTX.zip *
+move Azure_MQTT_Demo_MIMXRT1064-EVK_MW-Network_ETH_RTX.zip C:\Temp
+popd
+
+pushd Azure_MQTT_Demo\MIMXRT1064-EVK_WizFi360_WiFi_FreeRTOS
+"C:\Program Files\7-ZIP\7z" a -tzip Azure_MQTT_Demo_MIMXRT1064-EVK_WizFi360_WiFi_FreeRTOS.zip *
+move Azure_MQTT_Demo_MIMXRT1064-EVK_WizFi360_WiFi_FreeRTOS.zip C:\Temp
+popd
+
+pushd Azure_MQTT_Demo\MIMXRT1064-EVK_WizFi360_WiFi_RTX
+"C:\Program Files\7-ZIP\7z" a -tzip Azure_MQTT_Demo_MIMXRT1064-EVK_WizFi360_WiFi_RTX.zip *
+move Azure_MQTT_Demo_MIMXRT1064-EVK_WizFi360_WiFi_RTX.zip C:\Temp
+popd
+
+
+REM Creating Google Project ZIP Files
+
+pushd Google_MQTT_Demo\LPCXpresso55S69_ESP8266_WiFi_FreeRTOS
+"C:\Program Files\7-ZIP\7z" a -tzip Google_MQTT_Demo_LPCXpresso55S69_ESP8266_WiFi_FreeRTOS.zip *
+move Google_MQTT_Demo_LPCXpresso55S69_ESP8266_WiFi_FreeRTOS.zip C:\Temp
+popd
+
+pushd Google_MQTT_Demo\LPCXpresso55S69_ESP8266_WiFi_RTX
+"C:\Program Files\7-ZIP\7z" a -tzip Google_MQTT_Demo_LPCXpresso55S69_ESP8266_WiFi_RTX.zip *
+move Google_MQTT_Demo_LPCXpresso55S69_ESP8266_WiFi_RTX.zip C:\Temp
+popd
+
+pushd Google_MQTT_Demo\LPCXpresso55S69_ISM43362_WiFi_FreeRTOS
+"C:\Program Files\7-ZIP\7z" a -tzip Google_MQTT_Demo_LPCXpresso55S69_ISM43362_WiFi_FreeRTOS.zip *
+move Google_MQTT_Demo_LPCXpresso55S69_ISM43362_WiFi_FreeRTOS.zip C:\Temp
+popd
+
+pushd Google_MQTT_Demo\LPCXpresso55S69_ISM43362_WiFi_RTX
+"C:\Program Files\7-ZIP\7z" a -tzip Google_MQTT_Demo_LPCXpresso55S69_ISM43362_WiFi_RTX.zip *
+move Google_MQTT_Demo_LPCXpresso55S69_ISM43362_WiFi_RTX.zip C:\Temp
+popd
+
+pushd Google_MQTT_Demo\LPCXpresso55S69_WizFi360_WiFi_FreeRTOS
+"C:\Program Files\7-ZIP\7z" a -tzip Google_MQTT_Demo_LPCXpresso55S69_WizFi360_WiFi_FreeRTOS.zip *
+move Google_MQTT_Demo_LPCXpresso55S69_WizFi360_WiFi_FreeRTOS.zip C:\Temp
+popd
+
+pushd Google_MQTT_Demo\LPCXpresso55S69_WizFi360_WiFi_RTX
+"C:\Program Files\7-ZIP\7z" a -tzip Google_MQTT_Demo_LPCXpresso55S69_WizFi360_WiFi_RTX.zip *
+move Google_MQTT_Demo_LPCXpresso55S69_WizFi360_WiFi_RTX.zip C:\Temp
+popd
+
+pushd Google_MQTT_Demo\MIMXRT1064-EVK_ESP8266_WiFi_FreeRTOS
+"C:\Program Files\7-ZIP\7z" a -tzip Google_MQTT_Demo_MIMXRT1064-EVK_ESP8266_WiFi_FreeRTOS.zip *
+move Google_MQTT_Demo_MIMXRT1064-EVK_ESP8266_WiFi_FreeRTOS.zip C:\Temp
+popd
+
+pushd Google_MQTT_Demo\MIMXRT1064-EVK_ESP8266_WiFi_RTX
+"C:\Program Files\7-ZIP\7z" a -tzip Google_MQTT_Demo_MIMXRT1064-EVK_ESP8266_WiFi_RTX.zip *
+move Google_MQTT_Demo_MIMXRT1064-EVK_ESP8266_WiFi_RTX.zip C:\Temp
+popd
+
+pushd Google_MQTT_Demo\MIMXRT1064-EVK_MW-Network_ETH_FreeRTOS
+"C:\Program Files\7-ZIP\7z" a -tzip Google_MQTT_Demo_MIMXRT1064-EVK_ISM43362_MW-Network_ETH_FreeRTOS.zip *
+move Google_MQTT_Demo_MIMXRT1064-EVK_MW-Network_ETH_FreeRTOS.zip C:\Temp
+popd
+
+pushd Google_MQTT_Demo\MIMXRT1064-EVK_MW-Network_ETH_RTX
+"C:\Program Files\7-ZIP\7z" a -tzip Google_MQTT_Demo_MIMXRT1064-EVK_ISM43362_MW-Network_ETH_RTX.zip *
+move Google_MQTT_Demo_MIMXRT1064-EVK_MW-Network_ETH_RTX.zip C:\Temp
+popd
+
+pushd Google_MQTT_Demo\MIMXRT1064-EVK_WizFi360_WiFi_FreeRTOS
+"C:\Program Files\7-ZIP\7z" a -tzip Google_MQTT_Demo_MIMXRT1064-EVK_WizFi360_WiFi_FreeRTOS.zip *
+move Google_MQTT_Demo_MIMXRT1064-EVK_WizFi360_WiFi_FreeRTOS.zip C:\Temp
+popd
+
+pushd Google_MQTT_Demo\MIMXRT1064-EVK_WizFi360_WiFi_RTX
+"C:\Program Files\7-ZIP\7z" a -tzip Google_MQTT_Demo_MIMXRT1064-EVK_WizFi360_WiFi_RTX.zip *
+move Google_MQTT_Demo_MIMXRT1064-EVK_WizFi360_WiFi_RTX.zip C:\Temp
+popd
+
+
+REM Creating Paho Project ZIP Files
+
+pushd Paho_MQTT_Demo\LPCXpresso55S69_ESP8266_WiFi_FreeRTOS
+"C:\Program Files\7-ZIP\7z" a -tzip Paho_MQTT_Demo_LPCXpresso55S69_ESP8266_WiFi_FreeRTOS.zip *
+move Paho_MQTT_Demo_LPCXpresso55S69_ESP8266_WiFi_FreeRTOS.zip C:\Temp
+popd
+
+pushd Paho_MQTT_Demo\LPCXpresso55S69_ESP8266_WiFi_RTX
+"C:\Program Files\7-ZIP\7z" a -tzip Paho_MQTT_Demo_LPCXpresso55S69_ESP8266_WiFi_RTX.zip *
+move Paho_MQTT_Demo_LPCXpresso55S69_ESP8266_WiFi_RTX.zip C:\Temp
+popd
+
+pushd Paho_MQTT_Demo\LPCXpresso55S69_ISM43362_WiFi_FreeRTOS
+"C:\Program Files\7-ZIP\7z" a -tzip Paho_MQTT_Demo_LPCXpresso55S69_ISM43362_WiFi_FreeRTOS.zip *
+move Paho_MQTT_Demo_LPCXpresso55S69_ISM43362_WiFi_FreeRTOS.zip C:\Temp
+popd
+
+pushd Paho_MQTT_Demo\LPCXpresso55S69_ISM43362_WiFi_RTX
+"C:\Program Files\7-ZIP\7z" a -tzip Paho_MQTT_Demo_LPCXpresso55S69_ISM43362_WiFi_RTX.zip *
+move Paho_MQTT_Demo_LPCXpresso55S69_ISM43362_WiFi_RTX.zip C:\Temp
+popd
+
+pushd Paho_MQTT_Demo\LPCXpresso55S69_WizFi360_WiFi_FreeRTOS
+"C:\Program Files\7-ZIP\7z" a -tzip Paho_MQTT_Demo_LPCXpresso55S69_WizFi360_WiFi_FreeRTOS.zip *
+move Paho_MQTT_Demo_LPCXpresso55S69_WizFi360_WiFi_FreeRTOS.zip C:\Temp
+popd
+
+pushd Paho_MQTT_Demo\LPCXpresso55S69_WizFi360_WiFi_RTX
+"C:\Program Files\7-ZIP\7z" a -tzip Paho_MQTT_Demo_LPCXpresso55S69_WizFi360_WiFi_RTX.zip *
+move Paho_MQTT_Demo_LPCXpresso55S69_WizFi360_WiFi_RTX.zip C:\Temp
+popd
+
+pushd Paho_MQTT_Demo\MIMXRT1064-EVK_ESP8266_WiFi_FreeRTOS
+"C:\Program Files\7-ZIP\7z" a -tzip Paho_MQTT_Demo_MIMXRT1064-EVK_ESP8266_WiFi_FreeRTOS.zip *
+move Paho_MQTT_Demo_MIMXRT1064-EVK_ESP8266_WiFi_FreeRTOS.zip C:\Temp
+popd
+
+pushd Paho_MQTT_Demo\MIMXRT1064-EVK_ESP8266_WiFi_RTX
+"C:\Program Files\7-ZIP\7z" a -tzip Paho_MQTT_Demo_MIMXRT1064-EVK_ESP8266_WiFi_RTX.zip *
+move Paho_MQTT_Demo_MIMXRT1064-EVK_ESP8266_WiFi_RTX.zip C:\Temp
+popd
+
+pushd Paho_MQTT_Demo\MIMXRT1064-EVK_MW-Network_ETH_FreeRTOS
+"C:\Program Files\7-ZIP\7z" a -tzip Paho_MQTT_Demo_MIMXRT1064-EVK_ISM43362_MW-Network_ETH_FreeRTOS.zip *
+move Paho_MQTT_Demo_MIMXRT1064-EVK_MW-Network_ETH_FreeRTOS.zip C:\Temp
+popd
+
+pushd Paho_MQTT_Demo\MIMXRT1064-EVK_MW-Network_ETH_RTX
+"C:\Program Files\7-ZIP\7z" a -tzip Paho_MQTT_Demo_MIMXRT1064-EVK_ISM43362_MW-Network_ETH_RTX.zip *
+move Paho_MQTT_Demo_MIMXRT1064-EVK_MW-Network_ETH_RTX.zip C:\Temp
+popd
+
+pushd Paho_MQTT_Demo\MIMXRT1064-EVK_WizFi360_WiFi_FreeRTOS
+"C:\Program Files\7-ZIP\7z" a -tzip Paho_MQTT_Demo_MIMXRT1064-EVK_WizFi360_WiFi_FreeRTOS.zip *
+move Paho_MQTT_Demo_MIMXRT1064-EVK_WizFi360_WiFi_FreeRTOS.zip C:\Temp
+popd
+
+pushd Paho_MQTT_Demo\MIMXRT1064-EVK_WizFi360_WiFi_RTX
+"C:\Program Files\7-ZIP\7z" a -tzip Paho_MQTT_Demo_MIMXRT1064-EVK_WizFi360_WiFi_RTX.zip *
+move Paho_MQTT_Demo_MIMXRT1064-EVK_WizFi360_WiFi_RTX.zip C:\Temp
+popd
+
+
+REM Creating Watson Project ZIP Files
+
+pushd Watson_MQTT_Demo\MIMXRT1064-EVK_ESP8266_WiFi_FreeRTOS
+"C:\Program Files\7-ZIP\7z" a -tzip Watson_MQTT_Demo_MIMXRT1064-EVK_ESP8266_WiFi_FreeRTOS.zip *
+move Watson_MQTT_Demo_MIMXRT1064-EVK_ESP8266_WiFi_FreeRTOS.zip C:\Temp
+popd
+
+pushd Watson_MQTT_Demo\MIMXRT1064-EVK_ESP8266_WiFi_RTX
+"C:\Program Files\7-ZIP\7z" a -tzip Watson_MQTT_Demo_MIMXRT1064-EVK_ESP8266_WiFi_RTX.zip *
+move Watson_MQTT_Demo_MIMXRT1064-EVK_ESP8266_WiFi_RTX.zip C:\Temp
+popd
+
+pushd Watson_MQTT_Demo\MIMXRT1064-EVK_MW-Network_ETH_FreeRTOS
+"C:\Program Files\7-ZIP\7z" a -tzip Watson_MQTT_Demo_MIMXRT1064-EVK_ISM43362_MW-Network_ETH_FreeRTOS.zip *
+move Watson_MQTT_Demo_MIMXRT1064-EVK_MW-Network_ETH_FreeRTOS.zip C:\Temp
+popd
+
+pushd Watson_MQTT_Demo\MIMXRT1064-EVK_MW-Network_ETH_RTX
+"C:\Program Files\7-ZIP\7z" a -tzip Watson_MQTT_Demo_MIMXRT1064-EVK_ISM43362_MW-Network_ETH_RTX.zip *
+move Watson_MQTT_Demo_MIMXRT1064-EVK_MW-Network_ETH_RTX.zip C:\Temp
+popd
+
+pushd Watson_MQTT_Demo\MIMXRT1064-EVK_WizFi360_WiFi_FreeRTOS
+"C:\Program Files\7-ZIP\7z" a -tzip Watson_MQTT_Demo_MIMXRT1064-EVK_WizFi360_WiFi_FreeRTOS.zip *
+move Watson_MQTT_Demo_MIMXRT1064-EVK_WizFi360_WiFi_FreeRTOS.zip C:\Temp
+popd
+
+pushd Watson_MQTT_Demo\MIMXRT1064-EVK_WizFi360_WiFi_RTX
+"C:\Program Files\7-ZIP\7z" a -tzip Watson_MQTT_Demo_MIMXRT1064-EVK_WizFi360_WiFi_RTX.zip *
+move Watson_MQTT_Demo_MIMXRT1064-EVK_WizFi360_WiFi_RTX.zip C:\Temp
+popd
+
+REM Creating Blinky Project ZIP Files
+
+pushd Blinky\LPCXpresso55S69_FreeRTOS
+"C:\Program Files\7-ZIP\7z" a -tzip Blinky_LPCXpresso55S69_FreeRTOS.zip *
+move Blinky_LPCXpresso55S69_FreeRTOS.zip C:\Temp
+popd
+
+pushd Blinky\LPCXpresso55S69_RTX
+"C:\Program Files\7-ZIP\7z" a -tzip Blinky_LPCXpresso55S69_RTX.zip *
+move Blinky_LPCXpresso55S69_RTX.zip C:\Temp
+popd
+
+pushd Blinky\MIMXRT1064-EVK_FreeRTOS
+"C:\Program Files\7-ZIP\7z" a -tzip Blinky_MIMXRT1064-EVK_FreeRTOS.zip *
+move Blinky_MIMXRT1064-EVK_FreeRTOS.zip C:\Temp
+popd
+
+pushd Blinky\MIMXRT1064-EVK_RTX
+"C:\Program Files\7-ZIP\7z" a -tzip Blinky_MIMXRT1064-EVK_RTX.zip *
+move Blinky_MIMXRT1064-EVK_RTX.zip C:\Temp
+popd
+
