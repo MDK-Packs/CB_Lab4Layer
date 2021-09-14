fa15053c7ac62fddf764eba19b2fbd2134d88ca3
Robert Rostohar <robert.rostohar@arm.com>
2021-09-14 10:13:20 +0200

    Update extract layer script (add STM32H735G-DK board)

--- build/extract_layers.sh
+++ build/extract_layers.sh
@@ -83,6 +83,7 @@ mkdir "LPCXpresso55S69"
 mkdir "NUCLEO-L552ZE-Q"
 mkdir "NUCLEO-H743ZI"
 mkdir "STM32L562E-DK"
+mkdir "STM32H735G-DK"
 mkdir "STM32H745I-DISCO"
 popd
 
@@ -162,6 +163,10 @@ rm -r "${temp_path}"
 
 cbuildgen "${project_path}/Platform/NUCLEO-H743ZI/Platform.NUCLEO-H743ZI.cprj" extract --outdir="${temp_path}"
 cp -r "${temp_path}/Board/." "${layer_path}/Board/NUCLEO-H743ZI"
+rm -r "${temp_path}"
+
+cbuildgen "${project_path}/Platform/STM32H735G-DK/Platform.STM32H735G-DK.cprj" extract --outdir="${temp_path}"
+cp -r "${temp_path}/Board/." "${layer_path}/Board/STM32H735G-DK"
 rm -r "${temp_path}"
 
 cbuildgen "${project_path}/Platform/STM32L562E-DK/Platform.STM32L562E-DK.cprj" extract --outdir="${temp_path}"
